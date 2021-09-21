cb003867d471382fc27eff539efd060a57a26bf0
Tajammal Rasool <tajammalrasool8@gmail.com>
2021-09-21 22:59:19 +0500

    Friendship Calculator By TajammalRasool

---
+++ app/src/main/java/com/example/friendship/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.friendship;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int max=100;
+        int min=80;
+        final Random myRandom = new Random();
+
+
+        Button buttonGenerate = (Button)findViewById(R.id.generate);
+        final TextView textGenerateNumber = (TextView)findViewById(R.id.generatenumber);
+
+        buttonGenerate.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                textGenerateNumber.setText(String.valueOf(myRandom.nextInt(100)));
+            }
+        });
+
+        }
+}
